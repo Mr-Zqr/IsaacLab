a1f360ce20e8a4f7711393a0c448f75c097ecdf7
zqr-proart <qingray.zhao@gmail.com>
2025-02-26 10:49:53 +0800

    add g1 training code

--- source/isaaclab_tasks/isaaclab_tasks/direct/humanoid/__init__.py
+++ source/isaaclab_tasks/isaaclab_tasks/direct/humanoid/__init__.py
@@ -9,6 +9,7 @@ Humanoid locomotion environment.
 
 import gymnasium as gym
 
+from .g1_env import G1Env, G1EnvCfg
 from . import agents
 
 ##
@@ -25,4 +26,16 @@ gym.register(
         "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:HumanoidPPORunnerCfg",
         "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
     },
+)
+
+gym.register(
+    id="Isaac-G1-Direct-v0",
+    entry_point="isaaclab_tasks.direct.humanoid:G1Env",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": G1EnvCfg,
+        "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}:rsl_rl_ppo_cfg:HumanoidPPORunnerCfg",
+        "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml"
+    }
 )
